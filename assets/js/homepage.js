a7239ad9ac2ef4ef83d01f312bb32ba58caf1423
Sophia Barrett <sophia@yoursummit.media>
2021-10-10 16:23:11 -0700

    display search response data on page

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,7 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTermEl = document.querySelector("#repo-search-term")
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -22,9 +24,51 @@ var getUserRepos = function(user) {
     // make a request to the url
     fetch(apiURL).then(function(response) {
         response.json().then(function(data) {
-            console.log(data);
+            displayRepos(data, user);
         });
     });
+}
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+
+    // display search term
+    repoSearchTermEl.textContent = searchTerm;
+
+    // loop over repos
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+        // create container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        // creat a span element to hold repo name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count === 1) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i> " + repos[i].open_issues_count + " issue";
+        } else if (repos[i].open_issues_count > 1) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i> " + repos[i].open_issues_count + " issues";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+        // append to container
+        repoEl.appendChild(statusEl);
+
+        // append container to DOM
+        repoContainerEl.appendChild(repoEl);
+    }
 }
 
 userFormEl.addEventListener("submit", formSubmitHandler);
